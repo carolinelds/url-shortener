a397e940516fbf995615567f8aeb3a867996a747
carolinelds <carolinelds@gmail.com>
2024-11-06 01:47:25 -0300

    chore: remove comments

--- flask_app/shortly.py
+++ flask_app/shortly.py
@@ -5,12 +5,3 @@ application = create_app()
 
 if __name__ == "__main__":
     application.run(debug=True)
-
-# from app import db
-# import sqlalchemy as sa
-# import sqlalchemy.orm as so
-# from app.models.url import Url
-
-# @app.shell_context_processor
-# def make_shell_context():
-#     return {'sa': sa, 'so': so, 'db': db, 'Url': Url}
